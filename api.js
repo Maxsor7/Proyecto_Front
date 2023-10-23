86b7d5a13b321479776f14059ae2093d9664bc11
Mateo Pérez Bruno <102832289+Maxsor7@users.noreply.github.com>
2023-10-23 10:45:43 -0300

    Add files via upload

--- api.js
+++ api.js
@@ -1,11 +1,17 @@
 window.addEventListener('load', function(){
-    let table = document.querySelector(".table tbody"); // corrección aquí
+    let userTableBody = document.getElementById("userTableBody");
+    let addressTableBody = document.getElementById("addressTableBody");
+    let contactTableBody = document.getElementById("contactTableBody");
+    let addressTable = document.getElementById("addressTable");
+    let contactTable = document.getElementById("contactTable");
+    let userHeader = document.querySelector(".table thead tr");
 
-    let listUsers = async() =>{
+    let listUsers = async() => {
         let response = await fetch("https://jsonplaceholder.typicode.com/users");
         let data = await response.json();
+
         data.forEach(user => {
-            let row = document.createElement("tr");
+            let userRow = document.createElement("tr");
             let id = document.createElement("td");
             let name = document.createElement("td");
             let email = document.createElement("td");
@@ -16,14 +22,100 @@ window.addEventListener('load', function(){
             email.innerText = user.email;
             username.innerText = user.username;
 
-            row.appendChild(id);
-            row.appendChild(name);
-            row.appendChild(email);
-            row.appendChild(username);
+            userRow.appendChild(id);
+            userRow.appendChild(name);
+            userRow.appendChild(email);
+            userRow.appendChild(username);
 
-            table.appendChild(row);
+            userTableBody.appendChild(userRow);
+
+            let addressRow = document.createElement("tr");
+            let addressId = document.createElement("td");
+            let street = document.createElement("td");
+            let suite = document.createElement("td");
+            let city = document.createElement("td");
+            let zipcode = document.createElement("td");
+            let geo = document.createElement("td");
+
+            addressId.innerText = user.id;
+            street.innerText = user.address.street;
+            suite.innerText = user.address.suite;
+            city.innerText = user.address.city;
+            zipcode.innerText = user.address.zipcode;
+            geo.innerText = `${user.address.geo.lat}, ${user.address.geo.lng}`;
+
+            addressRow.appendChild(addressId);
+            addressRow.appendChild(street);
+            addressRow.appendChild(suite);
+            addressRow.appendChild(city);
+            addressRow.appendChild(zipcode);
+            addressRow.appendChild(geo);
+
+            addressTableBody.appendChild(addressRow);
+
+            let contactRow = document.createElement("tr");
+            let contactId = document.createElement("td");
+            let phone = document.createElement("td");
+            let website = document.createElement("td");
+            let company = document.createElement("td");
+
+            contactId.innerText = user.id;
+            phone.innerText = user.phone;
+            website.innerText = user.website;
+            company.innerText = user.company.name;
+
+            contactRow.appendChild(contactId);
+            contactRow.appendChild(phone);
+            contactRow.appendChild(website);
+            contactRow.appendChild(company);
+
+            contactTableBody.appendChild(contactRow);
         });
     };
 
-    listUsers(); // Llamar a la función aquí
+    let hideTables = () => {
+        userTableBody.style.display = "none";
+        addressTable.style.display = "none";
+        contactTable.style.display = "none";
+    };
+
+    let showAddresses = () => {
+        hideTables();
+        addressTable.style.display = "";
+
+        // Ocultar las columnas de ID, nombre, correo electrónico y nombre de usuario
+        userHeader.querySelectorAll("th")[0].style.display = "none"; // ID
+        userHeader.querySelectorAll("th")[1].style.display = "none"; // Name
+        userHeader.querySelectorAll("th")[2].style.display = "none"; // Email
+        userHeader.querySelectorAll("th")[3].style.display = "none"; // Username
+    };
+
+    let showContact = () => {
+        hideTables();
+        contactTable.style.display = "";
+
+        // Ocultar las columnas de ID, nombre, correo electrónico y nombre de usuario
+        userHeader.querySelectorAll("th")[0].style.display = "none"; // ID
+        userHeader.querySelectorAll("th")[1].style.display = "none"; // Name
+        userHeader.querySelectorAll("th")[2].style.display = "none"; // Email
+        userHeader.querySelectorAll("th")[3].style.display = "none"; // Username
+    };
+
+    let resetData = () => {
+        userTableBody.style.display = "";
+        addressTable.style.display = "none";
+        contactTable.style.display = "none";
+
+        // Mostrar las columnas de ID, nombre, correo electrónico y nombre de usuario
+        userHeader.querySelectorAll("th")[0].style.display = ""; // ID
+        userHeader.querySelectorAll("th")[1].style.display = ""; // Name
+        userHeader.querySelectorAll("th")[2].style.display = ""; // Email
+        userHeader.querySelectorAll("th")[3].style.display = ""; // Username
+    };
+
+    document.getElementById("filterButton").addEventListener("click", showAddresses);
+    document.getElementById("filterButton2").addEventListener("click", showContact);
+    document.getElementById("homeButton").addEventListener("click", resetData);
+
+    listUsers();
 });
